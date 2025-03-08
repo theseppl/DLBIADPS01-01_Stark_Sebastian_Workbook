eefc1d5bc6acae3a89aa9f82ceffef5bed77ad00
Sebastian Stark <sstark@gmx.net>
2025-03-08 13:09:52 +0100

    Aufgabe 6 in Haskell umgesetzt

---
+++ Aufgabe_6_Haskell.hs
@@ -0,0 +1,10 @@
+-- CountEvens.hs
+module CountEvens (countEvens) where
+
+-- Funktion zur Berechnung der Anzahl gerader Zahlen
+countEvens :: [Int] -> Int
+countEvens xs = length (filter even xs)
+
+main :: IO ()
+main = do
+    print (countEvens [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) -- Erwartet: 3
--- Aufgabe_6_Python.py
+++ Aufgabe_6_Python.py
@@ -7,7 +7,7 @@ class ZaehlGerade:
                 count += 1
         return count
 
-beispiel_liste = [1, 2, 3, 4, 5, 6]
+beispiel_liste = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 ergebnis = ZaehlGerade.zaehlen(beispiel_liste)
 print(f"Die Anzahl der geraden Zahlen in {beispiel_liste} ist: {ergebnis}")
 
