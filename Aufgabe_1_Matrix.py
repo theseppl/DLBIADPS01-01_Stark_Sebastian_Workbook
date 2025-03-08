499fcf9925a72238ebe3516207140794bb50f792
Sebastian Stark <sstark@gmx.net>
2025-03-08 08:04:54 +0100

    Unterscheidung gerichteter / ungerichteter Graph

--- Aufgabe_1_Matrix.py
+++ Aufgabe_1_Matrix.py
@@ -1,22 +1,25 @@
 class GraphAdjMatrix:
-    def __init__(self, num_nodes):
+    def __init__(self, num_nodes, directed=False):
         if num_nodes > 26:
             raise ValueError("Die maximale Anzahl der Knoten beträgt 26 (A bis Z).")
         self.matrix = [[0] * num_nodes for _ in range(num_nodes)]
         self.num_nodes = num_nodes
         self.node_labels = [chr(i) for i in range(65, 65 + num_nodes)]  # 'A' bis 'Z'
+        self.directed = directed  # Eigenschaft für gerichteten oder ungerichteten Graphen
 
     def add_edge(self, u, v):
         u_index = self.node_labels.index(u)
         v_index = self.node_labels.index(v)
         self.matrix[u_index][v_index] = 1
-        self.matrix[v_index][u_index] = 1  # Für ungerichtete Graphen
+        if not self.directed:
+            self.matrix[v_index][u_index] = 1  # Nur bei ungerichteten Graphen
 
     def remove_edge(self, u, v):
         u_index = self.node_labels.index(u)
         v_index = self.node_labels.index(v)
         self.matrix[u_index][v_index] = 0
-        self.matrix[v_index][u_index] = 0
+        if not self.directed:
+            self.matrix[v_index][u_index] = 0  # Nur bei ungerichteten Graphen
 
     def has_path(self, u, v):
         u_index = self.node_labels.index(u)
@@ -27,10 +30,9 @@ class GraphAdjMatrix:
         print("Adjazenzmatrix:")
         print("  " + " ".join(self.node_labels))  # Spaltenüberschriften
         for i, row in enumerate(self.matrix):
-            print(self.node_labels[i] + " " + " ".join(map(str, row)))  # Zeilenüberschriften
+            print(self.node_labels[i] + " " + " ".join(map(str, row)))
 
-
-matrix = GraphAdjMatrix(4)
+matrix = GraphAdjMatrix(4, True)
 matrix.add_edge('A','B')
 matrix.add_edge('A','A')
 matrix.display_matrix()
